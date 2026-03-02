7146e6f272800e33bf6f503c9070ccd28abd9cef
zaharikpro1505-dot <zaharikpro1505@gmail.com>
2026-03-02 21:22:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 function showInstruction() {
     const instructionDiv = document.getElementById("instruction");
-    instructionDiv.innerHTML = `
-        <h2>Как погладить Котофея:</h2>
-        <ol>
-            <li>Подойдите к Котофею.</li>
-            <li>Он начнёт просить пожрать.</li>
-            <li>Не давайте ему пожрать.</li>
-            <li>Пока Котофей спокойный, начните его гладить по голове и чесать за ушками.</li>
-            <li>Он снова будет просить пожрать.</li>
-            <li>Дайте ему пожрать.</li>
-        </ol>
-    `;
+
+    if (instructionDiv.innerHTML === "") {
+        instructionDiv.innerHTML = `
+            <h2>Как погладить Котофея:</h2>
+            <ol>
+                <li>Подойдите к Котофею.</li>
+                <li>Он начнёт просить пожрать.</li>
+                <li>Не давайте ему пожрать.</li>
+                <li>Пока Котофей спокойный, начните его гладить по голове и чесать за ушками.</li>
+                <li>Он снова будет просить пожрать.</li>
+                <li>Дайте ему пожрать.</li>
+            </ol>
+        `;
+    } else {
+        instructionDiv.innerHTML = "";
+    }
 }
